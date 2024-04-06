14cbdf644c8e68b87e28fbadad2ca64b72f4a401
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-06 18:59:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 let year =prompt("year");
 function daysOfAYear(year){
 	if((year%400===0) && (year%100===0)){
-		alert('366');
+		alert(366);
 	}
 	else if((year%4===0)&&(year%100!==0)){
-		alert('366');
+		alert(366);
 	}
 	else{
-		alert('365');
+		alert(365);
 	}
 }
 daysOfAYear(year);
